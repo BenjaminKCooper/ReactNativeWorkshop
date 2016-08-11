96324d7ee22c5d968c1778c129fd4dd58620b90c
Benjamin Cooper <benjaminkagancooper@gmail.com>
2016-08-11 15:52:17 -0400

    modified

--- GoneNative/index.ios.js
+++ GoneNative/index.ios.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 
 
 
 class GoneNative extends Component {
   render() {
     return (
-      <Text>{"\n"}Hello world!</Text>
+      // <Text style={{color: 'red'}}>{"\n"}Hello world!</Text>
+      <View>
+  <Text style={redStyle}>{"\n"}Hello world!</Text>
+  <Text style={redStyle}>Hey there</Text>
+  <Text style={redStyle}>Its a great day</Text>
+  <Text style={redStyle}>truly great</Text>
+</View>
     );
   }
+}
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+const redStyle = {
+  color: 'red'
 }
 
 AppRegistry.registerComponent('GoneNative',()=>GoneNative);
